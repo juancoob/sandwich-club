68c05a021a6702a86ff8f02bc1fce8057cee98ac
juancoob <ja.cobos.obrero@gmail.com>
2018-02-17 14:41:45 +0100

    Fix: Added good practices and null safety to JsonUtils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,25 +11,33 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String KEY_NAME = "name";
+    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    public static final String KEY_INGREDIENTS = "ingredients";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_PLACE_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+
     public static Sandwich parseSandwichJson(String json) throws JSONException {
         JSONObject jsonSandwich = new JSONObject(json);
-        JSONObject jsonSandwichName = jsonSandwich.getJSONObject("name");
-        JSONArray alsoKnownNames = jsonSandwichName.getJSONArray("alsoKnownAs");
-        JSONArray ingredients = jsonSandwich.getJSONArray("ingredients");
+        JSONObject jsonSandwichName = jsonSandwich.optJSONObject(KEY_NAME);
+        JSONArray alsoKnownNames = jsonSandwichName.optJSONArray(KEY_ALSO_KNOW_AS);
+        JSONArray ingredients = jsonSandwich.optJSONArray(KEY_INGREDIENTS);
         List<String> nameList = new ArrayList<>();
         List<String> ingredientList = new ArrayList<>();
         for (int sandwichNameIterator = 0; sandwichNameIterator < alsoKnownNames.length(); sandwichNameIterator++) {
-            nameList.add(String.valueOf(alsoKnownNames.get(sandwichNameIterator)));
+            nameList.add(alsoKnownNames.optString(sandwichNameIterator));
         }
         for (int ingredientsInterator = 0; ingredientsInterator < ingredients.length(); ingredientsInterator++) {
-            ingredientList.add(String.valueOf(ingredients.get(ingredientsInterator)));
+            ingredientList.add(ingredients.optString(ingredientsInterator));
         }
         Sandwich sandwich = new Sandwich();
-        sandwich.setMainName(jsonSandwichName.getString("mainName"));
+        sandwich.setMainName(jsonSandwichName.optString(KEY_MAIN_NAME));
         sandwich.setAlsoKnownAs(nameList);
-        sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
-        sandwich.setDescription(jsonSandwich.getString("description"));
-        sandwich.setImage(jsonSandwich.getString("image"));
+        sandwich.setPlaceOfOrigin(jsonSandwich.optString(KEY_PLACE_ORIGIN));
+        sandwich.setDescription(jsonSandwich.optString(KEY_DESCRIPTION));
+        sandwich.setImage(jsonSandwich.optString(KEY_IMAGE));
         sandwich.setIngredients(ingredientList);
         return sandwich;
     }
